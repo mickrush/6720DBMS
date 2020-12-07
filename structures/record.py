00163c18224aa5748827ea3791ded9680f8c2e9e
Riley <RileyMR@jncb.com>
2020-12-06 20:16:40 -0500

    Function definitions done for record and block

--- structures/record.py
+++ structures/record.py
@@ -5,10 +5,8 @@ class Record:
         self.values = []
 
 
-    def add(self, value1, value2, value3):
-        self.values.append(value1)
-        self.values.append(value2)
-        self.values.append(value3)
-
-    def update(self, column_number, value):
-        self.records[column_number] = value
+    def add(self):
+        pass
+    
+    def update(self):
+        pass
